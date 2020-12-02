385d46f58cd4990731c4509175167af75617a757
srp0030 <srp0030@uah.edu>
2020-12-02 00:29:56 -0600

    enemy die crash fixed

--- scene2.lua
+++ scene2.lua
@@ -13,7 +13,7 @@ local physics = require( "physics" )
 physics.start() --start physics here
 physics.setGravity( 0, 15 )
 
-local hiddenGroup = display.newGroup();
+local numEnemies = 0
 
 local soundTable = {
 	shootSound = audio.loadSound('shoot.wav');
@@ -23,7 +23,13 @@ local soundTable = {
 
 local ship_opt = {
     frames = {
-        {x = 364, y = 221, width = 86, height = 88}, -- frame 1
+        {x = 416, y = 245, width = 95, height = 100}, -- frame 1
+    }
+}
+
+local enemy_opt = {
+    frames = {
+        {x = 579, y = 832, width = 73, height = 94}, -- frame 1
     }
 }
 
@@ -33,54 +39,100 @@ local pillar_opt = {
     }
 }
 
-local ship_sheet = graphics.newImageSheet("shipsheet.png", ship_opt)
+local health_opt = {
+    frames = {
+        {x = 114, y = 40, width = 632, height = 148},
+        {x = 114, y = 226, width = 632, height = 148},
+        {x = 114, y = 410, width = 632, height = 148},
+        {x = 114, y = 595, width = 632, height = 148},
+        {x = 114, y = 779, width = 632, height = 148},
+        {x = 114, y = 964, width = 632, height = 148},
+    },
+}
+
+local ship_sheet = graphics.newImageSheet("ships_transparent.png", ship_opt)
+local enemy_sheet = graphics.newImageSheet("ships_transparent.png", enemy_opt)
+local health_sheet = graphics.newImageSheet("healthbar.png", health_opt)
 
 local ship_sequenceData = {
    {name = "walking", frames = {1}, time = 800, loopCount = 0}
 }
 
-local sheet = ship_sheet; 
-local sequenceData = ship_sequenceData;
-local player = display.newSprite(sheet, sequenceData) --initialize ship sprite
+local enemy_sequenceData = {
+   {name = "walking", frames = {1}, time = 800, loopCount = 0}
+}
+
+local health_sequenceData = {
+    {name = "health_decrease", frames = {1,2,3,4,5,6}, time = 800, loopCount = 0}
+}
+
+local player = display.newSprite(ship_sheet, ship_sequenceData) --initialize ship sprite
 player.tag = "player"
-player.HP = 3;
+player.HP = 10;
 
 local function switchScene(event) -- Change scenes
 
       composer.gotoScene("scene1") --Title Screen
 end 
+local bar = display.newSprite(health_sheet, health_sequenceData)
+bar:scale(0.5, 0.5)
+bar.x,y = 250, 100
+bar:setFrame(6)
+
+local function showHealth(event)
+    print(player.HP)
+    --bar:setSequence("health_decrease")
+    if(player.HP <= 8) then
+        bar:setFrame(5)
+    elseif(player.HP <= 6) then
+        bar:setFrame(4)
+    elseif(player.HP <= 4) then
+        bar:setFrame(3)
+    elseif(player.HP <= 3) then
+        bar:setFrame(2)
+    elseif(player.HP <= 2) then
+        bar:setFrame(1)
+    end
+end
+
+player:addEventListener("collision", showHealth)
+
 
 
 local enemies = {}; -- place to store the enemies
 local function createEnemy (yPos, id)
     id = 1
+    numEnemies = numEnemies +1 
     yPos = math.random(display.contentCenterY - 200, display.contentCenterY + 200)
-	local enemy = display.newRect (900, yPos, 100, 100);
-	enemy:setFillColor(0,1,0);
+    local enemy = display.newSprite(enemy_sheet, enemy_sequenceData);  --enemy sprite data
+    enemy.x= 1000
+    enemy.y = yPos
+	--enemy:setFillColor(0,1,0);
     physics.addBody(enemy,"dynamic", {isSensor = true});
     enemy.gravityScale = 0
 	enemy.tag = "enemy";
     enemy.HP = 3;
     enemy:setLinearVelocity( -100, 0 )
-    hiddenGroup:insert(enemy)
+    --hiddenGroup:insert(enemy)
     id = id+1  --increment enemy ID
 
     enemies[id] = enemy;
     
     local function enemyProjectile(event)  -- Timer calls function to fire bullets every second
     print("shoot")
-    local ebullet = display.newCircle(enemy.x-40, enemy.y, 5);
+    local ebullet = display.newCircle(enemies[id].x or 0, enemies[id].y or 0, 5);
     physics.addBody(ebullet, "kinematic", {radius=5, isSensor = true} );
     ebullet:setFillColor(1,0,0);
     ebullet:setLinearVelocity( -200, 0 )
     ebullet.tag = "enemyProj"
     end
-
-timer.performWithDelay(2000, enemyProjectile, -1)
+        if(numEnemies >= 1) then
+        timer.performWithDelay(2000, enemyProjectile, -1)
+        end
 end
 
 timer.performWithDelay( 5000, createEnemy, -1)
-timer.performWithDelay(6000, enemyProjectile, -1)
+--timer.performWithDelay(6000, enemyProjectile, -1)
 
 
 local function fire (event) -- handles player firing
@@ -89,6 +141,7 @@ local function fire (event) -- handles player firing
     physics.addBody(bullet, "kinematic", {radius=5} );
     bullet.isBullet = true
     bullet:setLinearVelocity( 200, 0 )
+    
 
     local function removeProjectile(event) --removes projectile on collision
         if (event.phase=="began") then
@@ -103,7 +156,8 @@ local function fire (event) -- handles player firing
 					event.other:setFillColor(event.other.HP/3, 0, 0)
 					audio.play(soundTable['hitSound']);
 				elseif (event.other.HP == 1) then
-					audio.play(soundTable['explodeSound']);
+                    audio.play(soundTable['explodeSound']);
+                    numEnemies = numEnemies -1
                     event.other:removeSelf(); 
                     event.other=nil;
 
@@ -123,7 +177,7 @@ local function playerHealth(event)
                     player.HP = player.HP -1;
                     print("hit")
 					audio.play(soundTable['hitSound']);
-            elseif (player.HP == 1) then
+            elseif (player.HP <= 1) then
                     event.other:removeSelf(); 
                     event.other=nil;
 					audio.play(soundTable['explodeSound']);
@@ -131,6 +185,19 @@ local function playerHealth(event)
                     composer.gotoScene("deathScene")
             end
     end
+    --[[if(event.other.tag == "topColumn" or event.other.tag == "bottomColumn") then
+
+        if (player.HP > 1) then
+                    player.HP = player.HP -1;
+                    print("hit")
+					audio.play(soundTable['hitSound']);
+            elseif (player.HP <= 1) then
+					audio.play(soundTable['explodeSound']);
+                    print("destroyed")
+                    composer.gotoScene("deathScene")
+        end
+    end--]]
+
 end
 
 player:addEventListener("collision", playerHealth)
@@ -153,7 +220,8 @@ local function addColumns()
 	
 	height = math.random(display.contentCenterY - 200, display.contentCenterY + 200)
 
-	local topColumn = display.newImageRect('column.png',300,714)
+    local topColumn = display.newImageRect('column.png',300,714)
+    topColumn.tag = "topColumn"
 	topColumn.anchorX = 0.5
 	topColumn.anchorY = 1
 	topColumn.x = display.contentWidth + 100
@@ -161,7 +229,8 @@ local function addColumns()
 	physics.addBody(topColumn, "static", {density=1, bounce=0.1, friction=.2, isSensor= true})
 	elements:insert(topColumn)
 	
-	local bottomColumn = display.newImageRect('column.png',300,714)
+    local bottomColumn = display.newImageRect('column.png',300,714)
+    bottomColumn.tag = "bottomColumn"
 	bottomColumn.anchorX = 0.5
 	bottomColumn.anchorY = 0
 	bottomColumn.x = display.contentWidth + 100
@@ -182,8 +251,6 @@ local function onObjectTouch(event)
     end
 end
 
-Runtime:addEventListener("touch", onObjectTouch)
-
 
 -- "scene:create()"
 function scene:create( event )
@@ -192,18 +259,25 @@ function scene:create( event )
 
        --sceneGroup:insert(enemy)
 
-local background = display.newImageRect( "space_background.png", 1920, 1080)
-background.x = 1920
-background.y = 1080
+local background = display.newImageRect( "space_background.png", display.contentWidth, display.contentHeight) 
+background.x = display.contentCenterX
+background.y = display.contentCenterY
 sceneGroup:insert(background)
 
-                                                            --Tested using button to allow user to shoot
-local button2 = display.newRect( 800, 700, 100, 75)
-button2:setFillColor(0,1,0)
+                                                            --invisible button to allow user to shoot
+local flyButton = display.newRect( 334, 375, 667, 750)
+flyButton:setFillColor(0,1,0, 0.01) --opacity set nearly to 0 to make invisible
 local buttontext2 = display.newText( "Shoot", 800, 700, native.systemFont, 16 )
-sceneGroup:insert(button2)
+sceneGroup:insert(flyButton)
 sceneGroup:insert(buttontext2)
-button2:addEventListener( "tap", fire )
+flyButton:addEventListener( "touch", onObjectTouch )
+
+local shootButton = display.newRect( 1000, 375, 667, 750)
+shootButton:setFillColor(0,0,1, 0.01) --opacity set nearly to 0 to make invisible
+local shootButtontext = display.newText( "fly", 100, 700, native.systemFont, 16 )
+sceneGroup:insert(shootButton)
+sceneGroup:insert(shootButtontext)
+shootButton:addEventListener( "tap", fire)
 
 
 local ground = display.newRect(display.contentCenterX, display.contentCenterY + 380, 1500, 1) -- Adds a ground object to stop the ship from falling off screen
